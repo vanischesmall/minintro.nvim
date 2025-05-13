1ff30db90c8b72c105fad397e2883d3bbaf66c60
Ivan Rubtsov <vanischesmall@gmail.com>
2025-05-14 01:04:16 +1000

    Update init.lua

--- lua/minintro/init.lua
+++ lua/minintro/init.lua
@@ -27,6 +27,13 @@ local intro_logo = {
 	"      ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═══╝",
 }
 
+local ffi = require("ffi")
+
+ffi.cdef([[
+void ui_busy_start(void);
+void ui_busy_stop(void);
+]])
+
 local PLUGIN_NAME = "ivan"
 local DEFAULT_COLOR = "#98c379"
 local INTRO_LOGO_HEIGHT = #intro_logo
@@ -92,25 +99,8 @@ end
 
 local function set_options()
 	-- vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:Cursor/lCursor,sm:block"		
-	local ffi = require("ffi")
-	
-	ffi.cdef([[
-	void ui_busy_start(void);
-	void ui_busy_stop(void);
-	]])
-	
-	vim.api.nvim_create_autocmd({"InsertEnter"}, {
-	  callback = function()
-	    ffi.C.ui_busy_start()
-	  end
-	})
-	
-	vim.api.nvim_create_autocmd({"InsertLeave"}, {
-	  callback = function()
-	    ffi.C.ui_busy_stop()
-	  end
-	})
-	
+
+	ffi.C.ui_busy_start()	
 	
 	vim.opt_local.cursorline = false     -- disable line highlighting
 	vim.opt_local.number = false         -- disable line numbers
